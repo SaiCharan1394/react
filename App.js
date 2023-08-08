3e669d8db2fe6843d85a1546df0bbee470300c2a
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-08-08 19:38:56 +0530

    changes

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
-const heading= <h1 id='heading'>react</h1>;
+
+
+const heading= <h1 id='heading'>React Element</h1>;
+const Heading2=()=>(<h1>Functional Component</h1>);
+const Heading3=()=>(<div><Heading2/><h1>Functional Component in an another Functional Component</h1> 
+ </div>);
 const root=ReactDOM.createRoot(document.getElementById('card'));
-root.render(heading);
+// root.render(heading);
+root.render(<Heading3/>);
