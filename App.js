57ed6086fdafbd38afe31c50e96b5a5dd41c1144
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-08-09 15:52:46 +0530

    changes

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
+const Header=()=>{
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className='logo' src="https://www.clipartmax.com/png/middle/112-1129793_healthy-food-logo-png.png"/>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+};
 
-const heading= <h1 id='heading'>React Element</h1>;
-const Heading2=()=>(<h1>Functional Component</h1>);
-const Heading3=()=>(<div><Heading2/><h1>Functional Component in an another Functional Component</h1> 
- </div>);
+
+const AppLayout=()=>{
+  return(
+    <div className="app">
+      <Header/>
+    </div>
+  )
+};
 const root=ReactDOM.createRoot(document.getElementById('card'));
-// root.render(heading);
-root.render(<Heading3/>);
+root.render(<AppLayout/>)
