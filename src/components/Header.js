d64c41710e4bb98c9b4ae5207e5f133516aad27d
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-08-21 18:40:41 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,32 @@ import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
-  const [btnName]=useState("Login")
-    return (
-      <div className="header">
-        <div>
-          <img
-            className="logo"
-            src={LOGO_URL}
-            alt="logo"
-          />
-        </div>
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Cart</li>
-            <button className="btn">{btnName}</button>
-          </ul>
-        </div>
+  const [btnName, setBtnName] = useState("Login");
+  return (
+    <div className="header">
+      <div>
+        <img className="logo" src={LOGO_URL} alt="logo" />
       </div>
-    );
-  };
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button
+            className="btn"
+            onClick={() => {
+              btnName === "Login"
+                ? setBtnName("Logout")
+                : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-
-  export default Header;
+export default Header;
