cccc50cf5c24bd7775b5191f3e7f7916f18059c3
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-08-25 13:12:37 +0530

    Update RestMenu.js

--- src/components/RestMenu.js
+++ src/components/RestMenu.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useState,useEffect } from "react";
+
 
 const RestaurantMenu = () => {
+    const [resInfo,setResInfo]=useState([])
   useEffect(() => {
     fetchMenu();
   }, []);
@@ -9,11 +11,13 @@ const RestaurantMenu = () => {
       "https://www.swiggy.com/mapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=16.3066525&lng=80.4365402&restaurantId=86549&submitAction=ENTER"
     );
     const json = await data.json();
-    console.log(json);
+    console.log(json.data.cards[3].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.itemCards[1])
+    setResInfo(json.data);
+    
   };
   return (
     <div>
-      <h1>Restaurant</h1>
+      <h1></h1>
       <h2>Menu</h2>
     </div>
   );
