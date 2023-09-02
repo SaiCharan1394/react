11e4b696a86b90cf705e227bc758452cc5098d10
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-09-02 10:39:23 +0530

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count] = useState(0);
   return (
     <div>
       <h1>Name: {name}</h1>
-      <h2>Location: Pileru</h2>
-      <h3>Mail: akkiraju@</h3>
+      <h1>Count: {count}</h1>
+      <h2>Location: Piler</h2>
+      <h3>Mail: akkiraju</h3>
     </div>
   );
 };
