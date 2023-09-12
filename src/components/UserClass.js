60f07bb4688517749b838f9aba28cb66794b3c43
SaiCharan1394 <105579195+SaiCharan1394@users.noreply.github.com>
2023-09-12 09:31:51 +0530

    Update UserClass.js

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,11 +3,11 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    this.state={
-      count:0
-    }
+    this.state = {
+      count: 0,
+    };
   }
-  
+
   render() {
     return (
       <div>
